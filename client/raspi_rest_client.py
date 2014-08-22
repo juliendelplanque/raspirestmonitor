5912d9183af269bbf00fc08c13a04c243365706c
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2014-08-22 23:34:01 +0200

    Inital commit.

---
+++ client/raspi_rest_client.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+""" This script contains functions for the REST client.
+    
+    Author: Julien Delplanque
+"""
+import http.client
+import json
+
+def get_pacman_pkgs_to_update(ip: str, username: str, passwd: str):
+    """ Get the list of packages from the REST server hosted by
+        the raspberry pi.
+
+        Keyword arguments:
+        ip       - the ip of the raspberry pi
+        username - your username
+        passwd   - your password
+    """
+    conn = http.client.HTTPConnection(ip+":5000")
+    conn.request("GET", "/pkgtoupdate")
+    response = conn.getresponse()
+    j = json.loads(response.read().decode("utf-8"))
+    return j.get("pacman")
+
+def get_sensors_data(ip: str, username: str, passwd: str):
+    """ Get the list of sensors data from the REST server hosted by
+        the raspberry pi.
+
+        Keyword arguments:
+        ip       - the ip of the raspberry pi
+        username - your username
+        passwd   - your password
+    """
+    conn = http.client.HTTPConnection(ip+":5000")
+    conn.request("GET", "/sensors")
+    response = conn.getresponse()
+    return json.loads(response.read().decode("utf-8"))
