1f1ca119dcaaea2186db7d13c5ee84b51834081b
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2014-08-23 00:01:01 +0200

    Add yaourt support.

--- client/raspi_rest_client.py
+++ client/raspi_rest_client.py
@@ -23,6 +23,23 @@ def get_pacman_pkgs_to_update(ip: str, username: str=None, passwd: str=None):
     j = json.loads(response.read().decode("utf-8"))
     return j.get("pacman")
 
+def get_yaourt_pkgs_to_update(ip: str, username: str=None, passwd: str=None):
+    """ Get the list of packages from the REST server hosted by
+        the raspberry pi.
+
+        TODO implement login.
+
+        Keyword arguments:
+        ip       - the ip of the raspberry pi
+        username - your username
+        passwd   - your password
+    """
+    conn = http.client.HTTPConnection(ip+":5000")
+    conn.request("GET", "/pkgtoupdate")
+    response = conn.getresponse()
+    j = json.loads(response.read().decode("utf-8"))
+    return j.get("yaourt")
+
 def get_sensors_data(ip: str, username: str=None, passwd: str=None):
     """ Get the list of sensors data from the REST server hosted by
         the raspberry pi.
