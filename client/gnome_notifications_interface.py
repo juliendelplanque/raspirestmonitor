cbf7a357b1b6817aaff442835b7b53d7cc1f0c6c
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2014-08-22 23:43:28 +0200

    Initial commit.

---
+++ client/gnome_notifications_interface.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+""" This script is an exemple of interface for the REST client.
+    It inform the user of changes using gnome's notifications.
+
+    Author: Julien Delplanque
+"""
+from gi.repository import Notify
+import time
+from raspi_rest_client import *
+
+GREETING="Dear sir,"
+SAY_GOODBYE="Sincerly, your raspberry pi."
+
+def format_message(pkgs: list):
+    """ Format a message to display to the user using the list of the packages
+        to update.
+
+        Keyword arguments:
+        pkgs - the list of package to update
+    """
+    message = GREETING + "\n"
+    message += "You have to know that these packages request an update:\n"
+    for pkg in pkgs:
+        message += "\t"+pkg + "\n "
+    message += "\n"+SAY_GOODBYE
+    return message
+
+def main():
+    """ Main function of this program.
+    """
+    pkgs_to_update = get_pacman_pkgs_to_update("192.168.0.10")
+    if len(pkgs_to_update) > 0:
+        message = format_message(pkgs_to_update)
+        Notify.init("Raspi updates")
+        notification = Notify.Notification.new("Raspi updates", message)
+        notification.show()
+        time.sleep(30) # The notification will stay 30 seconds
+        notification.close()
+
+if __name__ == '__main__':
+    main()
