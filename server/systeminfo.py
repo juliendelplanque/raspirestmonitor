0ecfa5688580f1af63c112617457eb2269180764
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2014-08-25 18:00:45 +0200

    Add get_uptime function.

---
+++ server/systeminfo.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+""" This script contains functions to access various system's info.
+
+    Author: Julien Delplanque
+"""
+import subprocess
+
+def get_uptime():
+    """ Return the uptime of the system as a str using the command: $ uptime
+    """
+    proc = subprocess.Popen(["uptime"], stdout=subprocess.PIPE, shell=True)
+    (output, error) = proc.communicate()
+    uptime = output.split()[2]
+    uptime = uptime[0:len(uptime)-2] # remove the comma
+    return uptime
