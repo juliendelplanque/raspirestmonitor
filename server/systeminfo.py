2942bb8ea8506db5f349010656756842916f4201
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2014-08-25 18:03:44 +0200

    Correct encoding bug.

--- server/systeminfo.py
+++ server/systeminfo.py
@@ -10,6 +10,6 @@ def get_uptime():
     """
     proc = subprocess.Popen(["uptime"], stdout=subprocess.PIPE, shell=True)
     (output, error) = proc.communicate()
-    uptime = output.split()[2]
+    uptime = output.decode("utf-8").split()[2]
     uptime = uptime[0:len(uptime)-2] # remove the comma
     return uptime
