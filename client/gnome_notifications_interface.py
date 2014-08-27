06155f09df01f8f0bbbb86d8c0eb58b07454efc8
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2014-08-27 13:14:15 +0200

    Update the program to match the changes in rasp_rest_client.

--- client/gnome_notifications_interface.py
+++ client/gnome_notifications_interface.py
@@ -29,10 +29,7 @@ def main():
     """ Main function of this program.
     """
     up_json = get_pkgs_to_update("192.168.0.10", 5000, "admin", "admin")
-    pacman_pkgs_to_update = extract_pacman_pkgs_to_update(up_json)
-    yaourt_pkgs_to_update = extract_yaourt_pkgs_to_update(up_json)
-    s_json = get_sensors_data("192.168.0.10", 5000, "admin", "admin")
-    print(s_json)
+    pacman_pkgs_to_update = extract_pacman_pkgs_to_update(up_json)    
     if len(pacman_pkgs_to_update) > 0:
         message = format_message(pacman_pkgs_to_update)
         Notify.init("Raspi updates")
